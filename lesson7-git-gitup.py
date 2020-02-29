6489e9537e8a321dec0fedd3508f2b188e0ea608
Armen281989 <armenmardoyan89@gmail.com>
2020-02-29 20:42:13 +0400

    'giturok'

---
+++ lesson7-git-gitup.py
@@ -0,0 +1,9 @@
+# create a now repository, stexcel nor repositoria
+# clone url, url kopia
+# cd ayntex vortex petq e stexcel
+# git status- imanal kargavijak
+# git add- file faili anun@ 
+# git commit -m 'anvanum'- git anun@
+# git push- bernum e
+# git add -A  bolor fayler miasin bernum
+# git pull  nerbernel fayl
