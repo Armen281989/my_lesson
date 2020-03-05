9d1542753f3770a18ba633618f8a1ff0139211fa
Armen281989 <armenmardoyan89@gmail.com>
2020-03-05 21:08:59 +0400

    'konverte'

---
+++ lesson8.py
@@ -0,0 +1,47 @@
+# # nahanj tari
+# i = int(input())
+# if i > 1582:
+# 	if i % 4 != 0:
+# 		print('no')
+# 	elif i % 100 == 0:
+# 		if i % 400 ==0:
+# 			print('yes')
+# 		else:
+# 			print('no')
+# 	else:
+# 		print('yes')		
+
+
+# #  mardu tariq hamematac mardu tariqi
+# print('cloys year')
+# age = int(input('cer tariq '))
+# if age <= 2:
+# 	print(age * 10.5)
+# else:
+# 	print((age - 2)* 4 + 21)
+
+
+#  # celsus konvert farenhayt
+# print('hashvum enq celsus ev farenhayt')
+# celsus = int(input('greq jermastijan celsusov '))
+# if celsus == 0:
+# 	print('farenhayt = 32')
+# else:
+# 	print(32 + 1.8 * celsus)	
+
+
+#  #  farenhayt konvert celsus (round) kloracnum e
+# faren = float(input('farenhayt = '))	
+# if faren == 32:
+# 	print('0 celsus')
+# else:
+# 	print(round(faren - 32) / 1.8, 'celsus')	
+
+# mail = input('greq cer mail ')
+# if '@' in mail:
+# 	print('jisht e')
+# else:
+# 	print('sxal')	
+
+# tnain
+# yeor name surename mail password
