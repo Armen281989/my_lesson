5419d3e1c04e9aa5d0e6c4254482c279d33079cf
Armen281989 <armenmardoyan89@gmail.com>
2020-03-07 20:59:25 +0400

    les9

---
+++ lesson9.py
@@ -0,0 +1,63 @@
+# loop
+
+# names = 'artur'
+# # for name in names:
+# # 	print(name)
+# #  kam
+# print(names[1])	
+
+# for i in range(10):
+# 	print('ani')
+
+# x = 0
+# for i in range (5):
+# 	x += 1
+# 	if x != 5:
+# 		print('right')
+# 	else:
+# 		print('error')
+
+# for x in range(2,10):
+# 	print(x)
+# for x in range(2,10,2):
+# 	print(x)
+
+# name = 'a'
+# lestname = 'papikyan'
+# for x  in name:
+# 	for y in lestname:
+# 		print(x,y)
+
+# i = 1
+# while i < 6:
+# 	print(i)
+# 	i += 1
+
+# i = 1
+# while i < 6:
+# 	print(i)
+# 	if i == 3:
+# 		break
+# 	i += 1
+  
+
+#   # zar xax
+# import random
+# comp = random.randrange(1,7)
+# while True:
+# 	user = random.randrange(1,7)
+# 	if user > comp:
+# 		print(user,comp,'menq haxtecinq')
+# 		break
+# 	elif comp == 6 and user == 6:
+# 		print(comp,user,'havasar')
+# 		break
+
+
+  #  continue hanum e tar
+name = 'arsen'
+for i in name:
+	if i == "r":
+		continue
+	print(i)
+
