bc2c6c507cec004d7ee939e888bd95ac0c10d558
Armen281989 <armenmardoyan89@gmail.com>
2020-03-12 21:03:34 +0400

    'less11'

---
+++ lesson11.py
@@ -0,0 +1,84 @@
+# fruits = ['banaa', 'apple', 'cherry','kiwi']
+# print(fruits)
+# print(fruits[1:3])
+# print(fruits[:3])
+# print(fruits[1:])
+# print(fruits[-3:-1])
+
+#  # change item value
+# fruits = ['banana', 'apple', 'cherry'] 
+# fruits[1] = 'kiwi'
+# print(fruits)
+
+
+# # len()  erkarutyun cuyc talis
+# fruits = ['banana', 'apple','cherry']
+# print(len(fruits))
+
+# # insert() avelacnume
+# fruits = ['banana', 'apple', 'cherry']
+# fruits.insert(1,'orange')
+# print(fruits)
+
+
+# remove hanume
+# fruits = ['banana','apple','cherry']
+# fruits.remove('apple')
+# print(fruits)
+
+
+# fruits = ['banana','apple','cherry']
+# c = fruits.pop()
+# print(fruits)
+# y = fruits.pop()
+# print(c)
+# print(y )
+# print(fruits)
+
+
+# # del
+# fruits = ['banana','apple','cherry']
+# del fruits[2]
+# # del fruits
+# print(fruits)
+
+# # extend
+# fruits = ['banana','apple','cherry']
+# numbers = [34,56,-45,7.23,-2.34]
+# fruits.extend(numbers)
+# print(fruits)
+
+# # sort() ajman kargov dasavorum e
+# numbers = [34,56,-45,7.23,-2.34]
+# numbers.sort()
+# print(numbers[-1])
+
+
+# # reverse
+# numbers = [34,56,-45,7.23,-2.34]
+# numbers.reverse()
+# print(numbers)
+
+
+
+numbers = [34,56,-45,7.23,-2.34,88,96,11]
+numbers.sort()
+print(numbers[-3:])
+print(numbers[:3])
+
+# numbers = [34,56,-456,7.56,-2.34]
+# print(numbers[1])
+
+#   # negativ indexsing
+# numbers = [34,56,-2.32,-5.45]
+# print(numbers[-1])
+
+
+
+
+# clas = ['artur','aram','ani','gurgen','arsen']
+
+# for i in clas:
+#     if i == 'artyom':
+#         print(i,'is in our class')
+# print('chka ayd anun')
